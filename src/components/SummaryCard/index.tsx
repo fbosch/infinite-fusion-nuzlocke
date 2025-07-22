4a0e2a75539529a75062c50f930bd0081ecbef60
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-07-22 18:54:08 +0200

    feat: added background to fusion sprite

--- src/components/SummaryCard/index.tsx
+++ src/components/SummaryCard/index.tsx
@@ -1,6 +1,7 @@
 import { EncounterData } from '@/loaders';
 import { FusionSprite } from './FusionSprite';
 import { type PokemonOption } from '@/loaders/pokemon';
+import { Fragment } from 'react';
 
 interface SummaryCardProps {
   encounterData: EncounterData;
@@ -37,23 +38,29 @@ export default function SummaryCard({ encounterData }: SummaryCardProps) {
 
   return (
     <div className='flex flex-col items-center justify-center relative'>
+      {encounterData.head || encounterData.body ? (
+        <Fragment>
+          <div
+            className='size-23 absolute -translate-y-2 rounded-lg dark:mix-blend-soft-light opacity-30 border border-gray-200 dark:border-gray-400'
+            style={{
+              background: `repeating-linear-gradient(white 1px, white 2px, rgba(154, 163, 175, 0.3) 1px, rgba(156, 163, 175, 0.3) 3px)`,
+            }}
+          />
+          <FusionSprite
+            encounterData={encounterData}
+            size='lg'
+            className='scale-150 relative'
+          />
+        </Fragment>
+      ) : null}
+
       {name && (
-        <div className=' text-center absolute top-0 -translate-y-7.5'>
+        <div className='z-5 text-center absolute bottom-0 translate-y-8'>
           <span className='text-sm font-medium font-mono truncate max-w-full block px-1 rounded text-gray-900 dark:text-white '>
             {name}
           </span>
         </div>
       )}
-      {encounterData.head || encounterData.body ? (
-        <div className='relative top-3'>
-          <div className='size-20 bg-gray-200 rounded-lg absolute inset-0 -translate-y-3 -translate-x-2 opacity-50' />
-          <FusionSprite
-            encounterData={encounterData}
-            size='lg'
-            className='scale-150 relative '
-          />
-        </div>
-      ) : null}
     </div>
   );
 }
