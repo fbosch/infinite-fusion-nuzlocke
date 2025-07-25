22aec6249683086a4af5503612d37a5e544d8c59
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-07-26 00:09:55 +0200

    refactor: simplify sprite service

--- src/services/spriteService.ts
+++ src/services/spriteService.ts
@@ -1,56 +1,23 @@
-import * as Comlink from 'comlink';
 import { SpriteService, generateSpriteUrl } from '@/lib/spriteCore';
 
-let mainThreadInstance: SpriteService | null = null;
-let instance: Comlink.Remote<SpriteService> | SpriteService | null = null;
-
-async function getInstance(mainThread = false) {
-  if (mainThread) {
-    if (!mainThreadInstance) {
-      mainThreadInstance = new SpriteService();
-    }
-    return mainThreadInstance as SpriteService;
-  }
-  if (instance) {
-    return instance;
-  }
-  try {
-    // Create worker and wrap with Comlink
-    const worker = new Worker(
-      new URL('@/workers/sprite.worker', import.meta.url)
-    );
-    const SpriteWorker = Comlink.wrap<typeof SpriteService>(worker);
-    // Create singleton instance
-    const spriteService = await new SpriteWorker();
-    instance = spriteService;
-  } catch (error) {
-    instance = mainThreadInstance;
-    console.error(error);
-  }
-  return instance || (mainThreadInstance as SpriteService);
-}
+const instance = new SpriteService();
 
 const service = {
   generateSpriteUrl,
   getArtworkVariants: async (
     ...args: Parameters<typeof SpriteService.prototype.getArtworkVariants>
   ): Promise<string[]> => {
-    const spriteService = await getInstance(true);
-    return spriteService.getArtworkVariants(...args);
+    return instance.getArtworkVariants(...args);
   },
   getPreferredVariant: async (
     ...args: Parameters<typeof SpriteService.prototype.getPreferredVariant>
   ): Promise<string | undefined> => {
-    // Use main thread instance for IndexedDB operations
-    const spriteService = await getInstance();
-    return spriteService.getPreferredVariant(...args);
+    return instance.getPreferredVariant(...args);
   },
   setPreferredVariant: async (
     ...args: Parameters<typeof SpriteService.prototype.setPreferredVariant>
   ): Promise<void> => {
-    // Use main thread instance for IndexedDB operations
-    const spriteService = await getInstance();
-    return spriteService.setPreferredVariant(...args);
+    return instance.setPreferredVariant(...args);
   },
 } as const;
 
