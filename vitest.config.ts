669ae0101b1f9008db20f751d0e627b1562de513
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-07-25 19:46:31 +0200

    test: progress on fixing tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,12 +2,10 @@
 import { defineConfig } from 'vitest/config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
     globals: true,
-    setupFiles: ['./src/test/setup.ts'],
     projects: [
       {
         test: {
@@ -27,9 +25,22 @@ export default defineConfig({
       },
       {
         test: {
+          name: 'react-hooks',
+          include: ['**/playthroughs.test.ts'],
+          environment: 'jsdom',
+        },
+      },
+      {
+        test: {
           name: 'node',
           include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
-          exclude: ['**/*.browser.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}', 'node_modules', 'dist', '.next'],
+          exclude: [
+            '**/*.browser.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}',
+            '**/playthroughs.test.ts',
+            'node_modules',
+            'dist',
+            '.next',
+          ],
           environment: 'node',
         },
       },
