0ffb3ea22543fa90b6af7836630f73699994899a
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-07-26 17:43:41 +0200

    feat: adjust favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -53,6 +53,23 @@ export default function RootLayout({
         <link rel='dns-prefetch' href='https://infinitefusiondex.com' />
         <link rel='preconnect' href='https://raw.githubusercontent.com' />
         <link rel='dns-prefetch' href='https://raw.githubusercontent.com' />
+        <link
+          rel='icon'
+          type='image/png'
+          href='/favicon-96x96.png'
+          sizes='96x96'
+        />
+        <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
+        <link rel='shortcut icon' href='/favicon.ico' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <meta
+          name='apple-mobile-web-app-title'
+          content='Infinite Fusion Nuzlocke Tracker'
+        />
       </head>
       <body className='antialiased font-sans'>
         <ThemeProvider>
