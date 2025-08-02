0693a4f8e48511cd052901b02905f216f12c106e
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-08-03 00:46:19 +0200

    fix: floating portal overflow

--- src/components/LocationTable/LocationTableHeader.tsx
+++ src/components/LocationTable/LocationTableHeader.tsx
@@ -10,7 +10,7 @@ export default function LocationTableHeader({
   headerGroups,
 }: LocationTableHeaderProps) {
   return (
-    <thead className='bg-gray-50 dark:bg-gray-800 sticky top-0 z-20 shadow-[0_0.5px_0_0_rgb(229,231,235)] dark:shadow-[0_0.5px_0_0_rgb(55,65,81)]'>
+    <thead className='bg-gray-50 dark:bg-gray-800 sticky top-0 z-100 shadow-[0_0.5px_0_0_rgb(229,231,235)] dark:shadow-[0_0.5px_0_0_rgb(55,65,81)]'>
       {headerGroups.map(headerGroup => (
         <tr key={headerGroup.id}>
           {headerGroup.headers.map(header => (
