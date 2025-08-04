6b4cb5fab7d9fd6a82a126cd438338a9bd2bf19d
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-08-04 02:21:30 +0200

    fix: build

--- src/components/ContextMenu/index.ts
+++
@@ -1,3 +0,0 @@
-export { default } from '../ContextMenu';
-export type { ContextMenuItem, ContextMenuProps } from '../ContextMenu.types';
-export { default as ContextMenuExample } from '../ContextMenuExample';
