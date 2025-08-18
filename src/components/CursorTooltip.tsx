29fdd0a0eb88305ef38450e5adda055296a1e162
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2025-08-18 22:10:23 +0200

    chore: minor adjustments

--- src/components/CursorTooltip.tsx
+++ src/components/CursorTooltip.tsx
@@ -248,7 +248,7 @@ export function CursorTooltip(props: CursorTooltipProps) {
             <div
               className={twMerge(
                 clsx(
-                  'rounded-md px-3 py-2 text-sm shadow-card w-max max-w-sm dark:pixel-shadow-black-25',
+                  'rounded-md px-3 py-2 text-sm shadow-elevation-4 w-max max-w-sm dark:pixel-shadow-black-25',
                   'pointer-events-none transform-gpu bg-white/75',
                   'dark:bg-gray-700/80 background-blur dark:text-white text-gray-700',
                   'border dark:border-gray-600 border-gray-200',
