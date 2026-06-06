2c8a833576585b9d8cc48dd3cbf155bffd280b35
Frederik Bosch <6979916+fbosch@users.noreply.github.com>
2026-06-06 09:04:51 +0200

    build(dev): add devenv config

---
+++ devenv.nix
@@ -0,0 +1,39 @@
+{ pkgs, ... }:
+
+let
+  no-mistakes = pkgs.buildGoModule rec {
+    pname = "no-mistakes";
+    version = "1.22.3";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "kunchenguid";
+      repo = "no-mistakes";
+      rev = "v${version}";
+      hash = "sha256-+8SPfOaQlG0oxSDsws9XqzqNnhjL/eA/nk+4osSxrck=";
+    };
+
+    vendorHash = "sha256-2pjiHVUwdQpXG9HTLW6wMZD+JpvFEcPMgBsVc6sck6w=";
+
+    subPackages = [ "cmd/no-mistakes" ];
+  };
+in
+{
+  languages.javascript = {
+    enable = true;
+    package = pkgs.nodejs-slim_22;
+    corepack.enable = true;
+  };
+
+  packages = [
+    pkgs.git
+    pkgs.gh
+    no-mistakes
+  ];
+
+  enterTest = ''
+    no-mistakes --version
+    git --version
+    gh --version
+    pnpm --version
+  '';
+}
